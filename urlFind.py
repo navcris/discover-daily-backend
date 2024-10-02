75a5608833ee62e6f8814647475a3770fefcdff1
Navarro Cristani <navcris@al-spro7-ap.univ.dir.wwu.edu>
2024-10-02 14:04:45 -0700

    change

--- urlFind.py
+++ urlFind.py
@@ -3,71 +3,13 @@ import requests
 
 
 
-def bbc_links():
-    
-    all_urls = []
-    url = 'https://www.bbc.com'
-    data = requests.get(url).text
-    soup = BeautifulSoup(data, features="html.parser")
-    for a in soup.find_all('a', href=True):
-        if a['href'] and a['href'][0] == '/' and a['href'] != '#':
-            a['href'] = url + a['href']
-        all_urls.append(a['href'])
-        
-    article_urls = [url for url in all_urls if bbc_is_article(url)]
-    
-    return article_urls
+"""
+Scrapes homepage of Yale news and stores article URLs in array
 
-def bbc_is_article(url, current_year='2024'):
-    if url:
-         if 'bbc.com' in url and '/article' in url:
-            return True
-    return False
+Args: N/A
 
-
-def cnn_links():
-    
-    all_urls = []
-    url = 'https://www.cnn.com'
-    data = requests.get(url).text
-    soup = BeautifulSoup(data, features="html.parser")
-    for a in soup.find_all('a', href=True):
-        if a['href'] and a['href'][0] == '/' and a['href'] != '#':
-            a['href'] = url + a['href']
-        all_urls.append(a['href'])
-        
-    article_urls = [url for url in all_urls if cnn_is_article(url)]
-    
-    return article_urls
-
-def cnn_is_article(url, current_year='2024'):
-    if url:
-         if 'cnn.com/{}/'.format(current_year) in url and '/gallery/' not in url:
-            return True
-    return False
-
-def ap_links():
-    
-    all_urls = []
-    url = 'https://www.apnews.com/'
-    data = requests.get(url).text
-    soup = BeautifulSoup(data, features="html.parser")
-    for a in soup.find_all('a', href=True):
-        if a['href'] and a['href'][0] == '/' and a['href'] != '#':
-            a['href'] = url + a['href']
-        all_urls.append(a['href'])
-        
-    article_urls = [url for url in all_urls if ap_is_article(url)]
-    
-    return article_urls
-
-def ap_is_article(url, keyword='article'):
-    if url:
-         if 'apnews.com' in url and '/{}/'.format(keyword) in url:
-            return True
-    return False
-
-
+Returns: N/A
+"""
 def yale_links():
     
     all_urls = []
@@ -83,12 +25,30 @@ def yale_links():
     
     return article_urls
 
-def yale_is_article(url, current_year='2024'):
+
+
+"""
+Checks if Yale url is an article
+
+Args: URL scraped from Yale Homepage
+
+Returns: Whether or not the url leads to an article
+"""
+def yale_is_article(url):
     if url:
          if 'e360.yale.edu' in url and '/features' in url:
             return True
     return False
 
+
+
+"""
+Scrapes homepage of Nature.com and stores article URLs in array
+
+Args: N/A
+
+Returns: N/A
+"""
 def nature_links():
     
     all_urls = []
@@ -105,59 +65,30 @@ def nature_links():
     
     return article_urls
 
-def nature_is_article(url, current_year='2024'):
+
+
+"""
+Checks if Nature url is an article
+
+Args: URL scraped from Nature Homepage
+
+Returns: Whether or not the url leads to an article
+"""
+def nature_is_article(url):
     if url:
          if 'nature.com' in url and '/articles' in url:
             return True
     return False
-  
-def science_links():
-    
-    all_urls = []
-    url = 'https://www.sciencenews.org/topic/space'
-    data = requests.get(url).text
-    soup = BeautifulSoup(data, features="html.parser")
-    
-    for a in soup.find_all('a', href=True):
-        if a['href'] and a['href'][0] == '/' and a['href'] != '#':
-            a['href'] = url + a['href']
-            
-        all_urls.append(a['href'])
-    
-    article_urls = [url for url in all_urls if science_is_article(url)]
-    
-    return article_urls
 
-def science_is_article(url, current_year='2024'):
-    if url:
-         if 'sciencenews.org' in url and '/article' in url:
-            return True
-    return False
-  
-def newsci_links():
-    
-    all_urls = []
-    url = 'https://www.newscientist.com/'
-    data = requests.get(url).text
-    soup = BeautifulSoup(data, features="html.parser")
-    
-    for a in soup.find_all('a', href=True):
-        if a['href'] and a['href'][0] == '/' and a['href'] != '#':
-            a['href'] = url + a['href']
-            
-        all_urls.append(a['href'])
-    
-    article_urls = [url for url in all_urls if newsci_is_article(url)]
-    
-    return article_urls
 
-def newsci_is_article(url, current_year='2024'):
-    if url:
-         if 'newscientist.com' in url and '/article' in url:
-            return True
-    return False
-  
 
+"""
+Scrapes homepage of ScienceNewsExplore and stores article URLs in array
+
+Args: N/A
+
+Returns: N/A
+"""
 def snex_links():
     
     all_urls = []
@@ -171,11 +102,20 @@ def snex_links():
             
         all_urls.append(a['href'])
     
-    article_urls = [url for url in all_urls if newsExpl_is_article(url)]
+    article_urls = [url for url in all_urls if snex_is_article(url)]
     
     return article_urls
 
-def newsExpl_is_article(url, current_year='2024'):
+
+
+"""
+Checks if ScienceNewsExplore url is an article
+
+Args: URL scraped from ScienceNewsExplore Homepage
+
+Returns: Whether or not the url leads to an article
+"""
+def snex_is_article(url):
     if url:
          if 'snexplores.org' in url and '/article/' in url:
             return True
